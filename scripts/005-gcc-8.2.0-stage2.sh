220c23a7de5212f1beac744af2d60206e2774027
AKuHAK <AKuHAK@gmail.com>
2018-12-25 22:52:14 +0200

    Remove clean from gcc

--- scripts/005-gcc-8.2.0-stage2.sh
+++ scripts/005-gcc-8.2.0-stage2.sh
@@ -48,4 +48,5 @@ mkdir build-$TARG_NAME-stage2 && cd build-$TARG_NAME-stage2 || { exit 1; }
 ../configure --quiet --prefix="$PS2DEVUJ/$TARG_NAME" --target="$TARGET" --enable-languages="c,c++" --disable-nls --disable-shared --disable-libssp --disable-libmudflap --disable-threads --disable-libgomp --disable-libquadmath --disable-target-libiberty --disable-target-zlib --without-ppl --without-cloog --with-headers=no --disable-libada --disable-libatomic --disable-multilib --with-headers="$PS2DEVUJ/$TARG_NAME/$TARGET/include" $TARG_XTRA_OPTS || { exit 1; }
 
 ## Compile and install.
-make --quiet clean && make --quiet -j $PROC_NR && make --quiet install && make --quiet clean || { exit 1; }
+# make --quiet clean && make --quiet -j $PROC_NR && make --quiet install && make --quiet clean || { exit 1; }
+make --quiet -j $PROC_NR && make --quiet install || { exit 1; }
