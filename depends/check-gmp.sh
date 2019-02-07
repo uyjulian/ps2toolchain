dff190c608e8e2716cc63560aa112922bdd6258a
astrelsky <ajs222@njit.edu>
2019-02-06 20:26:57 -0500

    Redirect stdout and stderr to /dev/null
    Modified the stdout redirects in the gmp, mpc, and mpfr scripts
    to also redirect stderr to /dev/null. This is only necessary for
    multiple checks before handling a 'file not found' exception.

--- depends/check-gmp.sh
+++ depends/check-gmp.sh
@@ -2,12 +2,12 @@
 # check-gmp.sh by uyjulian
 
 ## Check for the gmp library.
-ls /usr/include/gmp.h 1> /dev/null || \
-ls /usr/local/include/gmp.h 1> /dev/null || \
-ls /opt/include/gmp.h 1> /dev/null || \
-ls /opt/local/include/gmp.h 1> /dev/null || \
-ls /usr/include/x86_64-linux-gnu/gmp.h 1> /dev/null || \
-ls /usr/local/include/x86_64-linux-gnu/gmp.h 1> /dev/null || \
-ls /usr/include/i386-linux-gnu/gmp.h 1> /dev/null || \
-ls /usr/local/include/i386-linux-gnu/gmp.h 1> /dev/null || \
+ls /usr/include/gmp.h > /dev/null 2>&1 || \
+ls /usr/local/include/gmp.h > /dev/null 2>&1 || \
+ls /opt/include/gmp.h > /dev/null 2>&1 || \
+ls /opt/local/include/gmp.h > /dev/null 2>&1 || \
+ls /usr/include/x86_64-linux-gnu/gmp.h > /dev/null 2>&1 || \
+ls /usr/local/include/x86_64-linux-gnu/gmp.h > /dev/null 2>&1 || \
+ls /usr/include/i386-linux-gnu/gmp.h > /dev/null 2>&1 || \
+ls /usr/local/include/i386-linux-gnu/gmp.h > /dev/null 2>&1 || \
 { echo "ERROR: Install gmp before continuing."; exit 1; }
