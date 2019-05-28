279ea5342e615515830b11918b6d84504ddc3cc4
uyjulian <uyjulian@gmail.com>
2019-05-28 10:05:48 -0500

    Auto link with libraries

--- scripts/005-ps2sdk.sh
+++ scripts/005-ps2sdk.sh
@@ -35,5 +35,9 @@ find . -name Makefile\* -exec sed -i 's/\$(PS2DEV)/$(PS2DEVUJ)/g' '{}' \;
 make clean && make && make install && make clean || { exit 1; }
 
 ## Replace newlib's crt0 with the one in ps2sdk.
-# ln -sf "$PS2SDK/ee/startup/crt0.o" "$PS2DEV/ee/lib/gcc-lib/ee/3.2.3/crt0.o" || { exit 1; }
-# ln -sf "$PS2SDK/ee/startup/crt0.o" "$PS2DEV/ee/ee/lib/crt0.o" || { exit 1; }
+ln -sf "$PS2SDKUJ/ee/startup/crt0.o"  "$PS2DEVUJ/ee/mips64r5900el-ps2-elf/lib/crt0.o" || { exit 1; }
+
+## gcc needs to include both libc and libkernel from ps2sdk to be able to build executables.
+## NOTE: There are TWO libc libraries, gcc needs to include them both.
+ln -sf "$PS2SDKUJ/ee/lib/libc.a"      "$PS2DEVUJ/ee/mips64r5900el-ps2-elf/lib/libc.a" || { exit 1; }
+ln -sf "$PS2SDKUJ/ee/lib/libkernel.a" "$PS2DEVUJ/ee/mips64r5900el-ps2-elf/lib/libkernel.a" || { exit 1; }
