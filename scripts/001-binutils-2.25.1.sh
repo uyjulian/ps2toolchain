50741e3de9d66421b7d88d27b73c7602dd674b8b
uyjulian <uyjulian@gmail.com>
2019-12-27 19:11:13 -0600

    Binutils 2.25.1, GCC 5.5.0, newlib 3.1.0, and ps2sdk fork

--- scripts/001-binutils-2.25.1.sh
+++ scripts/001-binutils-2.25.1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# binutils-2.14.sh by Naomi Peori (naomi@peori.ca)
+# binutils-2.25.1.sh by Julian Uy (uyjulian@gmail.com)
+# Based on binutils-2.25.1.sh by SP193 (ysai187@yahoo.com)
+# Based on binutils-2.14.sh by Naomi Peori (naomi@peori.ca)
+# There is poor support for the "dvp" because I never worked with it
+# and don't actually understand why the old changes were necessary.
 
-BINUTILS_VERSION=2.14
+BINUTILS_VERSION=2.25.1
 ## Download the source code.
 SOURCE=http://ftpmirror.gnu.org/binutils/binutils-$BINUTILS_VERSION.tar.bz2
 wget --continue $SOURCE || { exit 1; }
@@ -12,10 +16,14 @@ rm -Rf binutils-$BINUTILS_VERSION && tar xfj binutils-$BINUTILS_VERSION.tar.bz2 
 
 ## Enter the source directory and patch the source code.
 cd binutils-$BINUTILS_VERSION || { exit 1; }
+
 if [ -e ../../patches/binutils-$BINUTILS_VERSION-PS2.patch ]; then
 	cat ../../patches/binutils-$BINUTILS_VERSION-PS2.patch | patch -p1 || { exit 1; }
 fi
-cat ../../patches/binutils-$BINUTILS_VERSION-add-64bit-mingw-w64-toolchain-support.patch | patch -p1 || { exit 1; }
+
+target_names=("ee" "iop" "dvp")
+targets=("mips64r5900el-ps2-elf" "mipsel-ps2-irx" "dvp")
+extra_opts=("--with-float=hard" "" "--with-float=hard")
 
 OSVER=$(uname)
 if [ ${OSVER:0:10} == MINGW64_NT ]; then
@@ -36,19 +44,19 @@ fi
 echo "Building with $PROC_NR jobs"
 
 ## For each target...
-for TARGET in "ee" "iop" "dvp"; do
+for ((i=0; i<${#target_names[@]}; i++)); do
+	TARG_NAME=${target_names[i]}
+	TARGET=${targets[i]}
+	TARG_XTRA_OPTS=${extra_opts[i]}
+
 	## Create and enter the build directory.
-	mkdir build-$TARGET && cd build-$TARGET || { exit 1; }
+	mkdir build-$TARG_NAME && cd build-$TARG_NAME || { exit 1; }
 
 	## Configure the build.
-	if [ ${OSVER:0:6} == Darwin ]; then
-		CC=/usr/bin/gcc CXX=/usr/bin/g++ LD=/usr/bin/ld CFLAGS="-O0 -ansi -Wno-implicit-int -Wno-return-type" ../configure --quiet --disable-build-warnings --prefix="$PS2DEV/$TARGET" --target="$TARGET" $TARG_XTRA_OPTS || { exit 1; }
-	else
-		../configure --quiet --disable-build-warnings --prefix="$PS2DEV/$TARGET" --target="$TARGET" $TARG_XTRA_OPTS || { exit 1; }
-	fi
+	../configure --prefix="$PS2DEV/$TARG_NAME" --target="$TARGET" $TARG_XTRA_OPTS || { exit 1; }
 
 	## Compile and install.
-	make --quiet clean && make --quiet -j $PROC_NR CFLAGS="$CFLAGS -D_FORTIFY_SOURCE=0" && make --quiet install && make --quiet clean || { exit 1; }
+	make --quiet clean && make --quiet -j $PROC_NR && make --quiet install && make --quiet clean || { exit 1; }
 
 	## Exit the build directory.
 	cd .. || { exit 1; }
---
+++ scripts/002-gcc-5.5.0-stage1.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# gcc-5.5.0-stage1.sh by Julian Uy (uyjulian@gmail.com)
+# Based on gcc-5.3.0-stage1.sh by SP193 (ysai187@yahoo.com)
+# Based on gcc-3.2.3-stage1.sh by AKuHAK
+# Based on gcc-3.2.2-stage1.sh by Naomi Peori (naomi@peori.ca)
+
+GCC_VERSION=5.5.0
+ISL_VERSION=0.18
+## Download the source code.
+GCC_SOURCE=http://ftpmirror.gnu.org/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz
+wget --continue $GCC_SOURCE || { exit 1; }
+ISL_SOURCE=http://isl.gforge.inria.fr/isl-$ISL_VERSION.tar.xz
+wget --continue $ISL_SOURCE || { exit 1; }
+
+## Unpack the source code.
+echo Decompressing GCC $GCC_VERSION. Please wait.
+rm -Rf gcc-$GCC_VERSION && tar xfJ gcc-$GCC_VERSION.tar.xz || { exit 1; }
+echo Decompressing ISL $ISL_VERSION. Please wait.
+rm -Rf isl-$ISL_VERSION && tar xfJ isl-$ISL_VERSION.tar.xz || { exit 1; }
+
+## Enter the source directory and patch the source code.
+cd gcc-$GCC_VERSION || { exit 1; }
+
+## Link ISL for in tree build.
+ln -s ../isl-$ISL_VERSION isl
+
+## Apply the uncommitted patches first (libgcc, short-loop bug, EE-core extensions, MMI)
+## TODO: Remove these lines, once the patches are submitted.
+cat ../../patches/gcc-$GCC_VERSION-libgcc.patch | patch -p1 || { exit 1; }
+cat ../../patches/gcc-$GCC_VERSION-ee.patch | patch -p1 || { exit 1; }
+cat ../../patches/gcc-$GCC_VERSION-mmi.patch | patch -p1 || { exit 1; }
+
+if [ -e ../../patches/gcc-$GCC_VERSION-PS2.patch ]; then
+	cat ../../patches/gcc-$GCC_VERSION-PS2.patch | patch -p1 || { exit 1; }
+fi
+
+target_names=("ee" "iop")
+targets=("mips64r5900el-ps2-elf" "mipsel-ps2-irx")
+extra_opts=("--with-float=hard --with-newlib" "--disable-libstdcxx")
+
+OSVER=$(uname)
+## Apple needs to pretend to be linux
+if [ ${OSVER:0:6} == Darwin ]; then
+	TARG_XTRA_OPTS="--build=i386-linux-gnu --host=i386-linux-gnu"
+elif [ ${OSVER:0:10} == MINGW64_NT ]; then
+	TARG_XTRA_OPTS="--build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32"
+else
+	TARG_XTRA_OPTS=""
+fi
+
+## Determine the maximum number of processes that Make can work with.
+if [ ${OSVER:0:5} == MINGW ]; then
+	PROC_NR=$NUMBER_OF_PROCESSORS
+elif [ ${OSVER:0:6} == Darwin ]; then
+	PROC_NR=$(sysctl -n hw.ncpu)
+else
+	PROC_NR=$(nproc)
+fi
+
+echo "Building with $PROC_NR jobs"
+
+## For each target...
+for ((i=0; i<${#target_names[@]}; i++)); do
+	TARG_NAME=${target_names[i]}
+	TARGET=${targets[i]}
+	TARG_XTRA_OPTS=${extra_opts[i]}
+
+	## Create and enter the build directory.
+	mkdir build-$TARG_NAME-stage1 && cd build-$TARG_NAME-stage1 || { exit 1; }
+
+	## Configure the build.
+	../configure --prefix="$PS2DEV/$TARG_NAME" --target="$TARGET" --enable-languages="c" --disable-nls --disable-shared --disable-libssp --disable-libmudflap --disable-threads --disable-libgomp --disable-libquadmath --disable-target-libiberty --disable-target-zlib --without-ppl --without-cloog --with-headers=no --disable-libada --disable-libatomic --disable-multilib --with-isl $TARG_XTRA_OPTS || { exit 1; }
+
+	## Compile and install.
+	make --quiet clean && make --quiet -j $PROC_NR && make --quiet install && make --quiet clean || { exit 1; }
+
+	## Exit the build directory.
+	cd .. || { exit 1; }
+
+	## End target.
+done
